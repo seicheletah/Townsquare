43964983ce1de75f8e35902543930d5859cda9d5
Sannik Biswas <sannikbiswas@gmail.com>
2025-08-30 19:16:11 +0530

    added semi functional login form. Also updated routes,base,index.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import townsquare
 from app.forms import LoginForm
 
-@townsquare.route('/')
-@townsquare.route('/index')
+@townsquare.route('/', methods=['GET', 'POST'])
 
 def index():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/')
     return render_template('index.html', title='Townsquare', form=form)
